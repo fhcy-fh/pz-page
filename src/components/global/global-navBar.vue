edd5eb80ee98c13511ea81f19116770567d8535e
fenghao <fh@fhcy.vip>
2025-01-08 23:57:20 +0800

    feat:el-text修改为p

--- src/components/global/global-navBar.vue
+++ src/components/global/global-navBar.vue
@@ -1,29 +1,30 @@
 <template>
-    <van-nav-bar :style="{ backgroundColor: '#F56C6C', color: '#000' }" @click-left="handleBack">
-        <template #left v-if="props.showBack">
-            <van-icon name="arrow-left" :style="{ color: '#fff' }" size="5vw" />
-            <el-text :style="{ color: '#fff' }" size="large">返回</el-text>
-        </template>
-        <template #title>
-            <el-text :style="{ color: '#fff' }" size="large">{{ props.title }}</el-text>
-        </template>
-    </van-nav-bar>
+  <van-nav-bar :style="{ backgroundColor: '#F56C6C', color: '#000' }" @click-left="handleBack">
+    <template #left v-if="props.showBack">
+      <van-icon name="arrow-left" :style="{ color: '#fff' }" size="5vw"/>
+      <p style="color: #fff">返回</p>
+    </template>
+    <template #title>
+      <p style="color: #fff">{{ props.title }}</p>
+    </template>
+  </van-nav-bar>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
+import {useRouter} from 'vue-router';
 
 interface Props {
-    title: string,
-    showBack: boolean
+  title: string,
+  showBack: boolean
 }
+
 const props = defineProps<Props>();
 
 // 获取路由器实例
 const router = useRouter();
 
 const handleBack = () => {
-    router.back()
+  router.back()
 }
 </script>
 
