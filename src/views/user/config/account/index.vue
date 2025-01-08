6e24ee86366a12bb51f99ff56bcdce5f486c9b2b
fenghao <fh@fhcy.vip>
2025-01-08 23:19:37 +0800

    feat:账号配置优化

--- src/views/user/config/account/index.vue
+++ src/views/user/config/account/index.vue
@@ -1,53 +1,52 @@
 <template>
-  <div style="margin-top: 1vh">
-    <van-loading
-        v-if="this_loading_get_all"
-        color="#1989fa"
-        size="8vw"
-        vertical>
-      加载中
-    </van-loading>
-    <van-swipe-cell v-else v-for="item in this_account_list">
-      <van-cell :title="item.name" :value="item.last_amount" @click="this_func_show_update(item)"/>
-      <template #right>
-        <van-button square type="danger" text="删除"/>
-      </template>
-    </van-swipe-cell>
-    <van-button plain hairline
-                type="primary"
-                @click="this_func_show_save"
-                style="width: 80vw; margin-top: 2vh; margin-left: 10vw; margin-right: 10vw">
-      新增
-    </van-button>
+  <div style="margin-top: 1vh; height: 100%; background-color: #fff">
+    <van-pull-refresh :model-value="this_loading_get_all" @refresh="this_func_get_all" style="height: 100%">
+      <van-swipe-cell v-for="item in this_account_list">
+        <van-cell :title="item.name" :value="item.last_amount" @click="this_func_show_update(item)"/>
+        <template #right>
+          <van-button square type="danger" text="删除"/>
+        </template>
+      </van-swipe-cell>
+      <van-button
+          plain
+          hairline
+          type="primary"
+          @click="this_func_show_save"
+          style="width: 80vw; margin-top: 2vh; margin-left: 10vw; margin-right: 10vw">
+        新增
+      </van-button>
 
-    <van-dialog
-        v-model:show="this_show_save"
-        title="新增账户"
-        @confirm="this_func_save"
-        show-cancel-button
-        close-on-click-overlay>
-      <van-cell-group inset>
-        <van-field
-            v-model="this_account_save_dto.name"
-            label="账户"
-            placeholder="账户名"
-        />
-      </van-cell-group>
-    </van-dialog>
-    <van-dialog
-        v-model:show="this_show_update"
-        title="编辑账户"
-        @confirm="this_func_update"
-        show-cancel-button
-        close-on-click-overlay>
-      <van-cell-group inset>
-        <van-field
-            v-model="this_account_update_dto.name"
-            label="账户"
-            placeholder="账户名"
-        />
-      </van-cell-group>
-    </van-dialog>
+      <van-dialog
+          v-model:show="this_show_save"
+          title="新增账户"
+          @confirm="this_func_save"
+          show-cancel-button
+          close-on-click-overlay>
+        <van-cell-group inset>
+          <van-field
+              v-model="this_account_save_dto.name"
+              label="账户"
+              placeholder="账户名"
+          />
+        </van-cell-group>
+      </van-dialog>
+      <van-dialog
+          v-model:show="this_show_update"
+          title="编辑账户"
+          @confirm="this_func_update"
+          show-cancel-button
+          close-on-click-overlay>
+        <van-cell-group inset>
+          <van-field
+              v-model="this_account_update_dto.name"
+              label="账户"
+              placeholder="账户名"
+          />
+        </van-cell-group>
+      </van-dialog>
+    </van-pull-refresh>
+
+
   </div>
 </template>
 
@@ -58,7 +57,7 @@ import {api_account_getAll, api_account_save, api_account_update} from "@/api/ap
 
 const this_show_save = ref(false);
 const this_show_update = ref(false);
-const this_loading_get_all = ref(false);
+const this_loading_get_all = ref<boolean>(false);
 
 const this_account_list = ref<AccountDto[]>();
 const this_account_save_dto = ref<AccountSaveDto>({
