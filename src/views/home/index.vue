bd657bf8cc14a260fc48622617c05e13377f4a09
fenghao <fh@fhcy.vip>
2025-01-09 21:27:26 +0800

    feat:首页调整

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <div style="padding-top: 2vh; padding-left: 1vw; padding-right: 1vw;">
-    <van-grid direction="horizontal" :column-num="3" :gutter="1">
+  <div style="padding-left: 1vw; padding-right: 1vw;">
+    <van-notice-bar
+        left-icon="volume-o"
+        text="功能持续更新中。"
+    />
+    <van-grid direction="horizontal" :column-num="3" :gutter="1" style="margin-top: 1vh;">
       <van-grid-item icon="gold-coin-o" text="账户结算" to="/"/>
     </van-grid>
   </div>
