5aea29e038d831fdad29c3be7afa6084c656dce4
fenghao <fh@fhcy.vip>
2025-01-12 18:25:19 +0800

    feat:账户明细调整

--- src/views/account/daily/index.vue
+++ src/views/account/daily/index.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>
-    <van-tabs v-model:active="this_account_code" animated :ellipsis="false">
-      <van-tab v-for="item in this_account_list" :title="item.name" :name="item.code" @click-tab="this_func_get_account_daily_list">
+  <div style="height: 100vh; overflow-y: auto;">
+    <van-tabs v-model:active="this_account_code"
+              :ellipsis="false"
+              animated
+              sticky
+              offset-top="6vh"
+              @click-tab="this_func_get_account_daily_init">
+      <van-tab v-for="item in this_account_list" :title="item.name" :name="item.code">
         <van-list
             v-model:loading="this_account_list_get_loading"
             :finished="this_account_list_get_finished"
             finished-text="没有更多了"
-            @load="this_func_get_account_daily_list"
+            @load="this_func_get_account_daily_list_page"
         >
-          <van-cell v-for="item in this_account_daily_list" :key="item.date" :title="item.amount"/>
+          <van-cell v-for="item in this_account_daily_list"
+                    :key="item.date"
+                    :title="item.date"
+                    :value="'¥' + item.amount"/>
         </van-list>
       </van-tab>
     </van-tabs>
@@ -29,12 +37,23 @@ const this_account_daily_list = ref<AccountDailyDto[]>([]);
 
 const this_page_number = ref(1);
 
-const this_func_get_account_daily_list = async () => {
+const this_func_get_account_daily_init = async () => {
+  console.log('初始化')
+  this_page_number.value = 1
+  this_account_daily_list.value = []
+  this_account_list_get_finished.value = false
+  // await this_func_get_account_daily_list_page()
+}
+const this_func_get_account_daily_list_page = async () => {
   const res = await api_account_daily_get(this_account_code.value, this_page_number.value);
-  if (Array.isArray(res.data)) {
-    this_account_daily_list.value.push(res.data)
+  if (res.data.length > 0) {
+    this_account_daily_list.value.push(...res.data)
   }
-  this_account_list_get_finished.value = true
+  if (res.data.length < 10) {
+    this_account_list_get_finished.value = true
+  }
+  this_account_list_get_loading.value = false;
+  this_page_number.value = this_page_number.value + 1;
 }
 
 onMounted(async () => {
